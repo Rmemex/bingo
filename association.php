f242bf2c84bb790be3b0f54017d2a74978abb17d
Mendrika <102210087+Rmemex@users.noreply.github.com>
2025-03-18 22:12:55 +0300

    up

--- association.php
+++ association.php
@@ -66,7 +66,6 @@
             </footer>
         </div>
     </div>
-
     <script src="assets/plugins/jquery/jquery.min.js"></script>
     <script src="assets/plugins/jquery-ui/jquery-ui.min.js"></script>
     <script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
