3976bd50d11989b6deaf1eef8fe9cb6f16f3c56f
Mendrika <102210087+Rmemex@users.noreply.github.com>
2025-03-18 22:16:15 +0300

    up

--- config.php
+++ config.php
@@ -1,16 +1,8 @@
 <?php
 
-
-
-// Configurations de la base de données
-
-define('DB_HOST', 'localhost');  // Mettez l'hôte de votre base de données
-
-define('DB_NAME', 'bingo');  // Mettez le nom de votre base de données
-
-define('DB_USER', 'root');  // Mettez le nom d'utilisateur de votre base de données
-
-define('DB_PASSWORD', '');  // Mettez le mot de passe de votre base de données
-
+define('DB_HOST', 'localhost');  
+define('DB_NAME', 'bingo'); 
+define('DB_USER', 'root'); 
+define('DB_PASSWORD', '');  
 
 
